bc5c92d942c2d745e4fb9731a25ba2463d9f6971
Fullstack-Jack-33 <Jaground@gmail.com>
2019-07-14 15:28:13 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,20 +3,19 @@ console.log("Up and running!")
 let cards = ["queen", "queen", "king", "king"]; 
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log('User flipped ' + cardOne);
-
-let cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log('User flipped ' + cardTwo);
-
-if (cardsInPlay.length === 2) {
-    if ( cardsInPlay[0] === cardsInPlay[1]){
-        alert('You found a match!');
-    }
-    else {
-        alert('Sorry, try again');
+function checkForMatch() {
+    if (cardsInPlay.length === 2) {
+        let result = cardsInPlay[0] === cardsInPlay[1] ? 'You found a match!' : 'Sorry, try again.';
+        console.log(result);
+        //alert(result);
     }
 }
 
+function flipCard(cardId) {
+    cardsInPlay.push(cards[cardId]);
+    console.log('User flipped ' + cards[cardId]);
+    checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
